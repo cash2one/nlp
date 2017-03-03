efff5fe1d3148849907e6d5dc5bec6a810e423b6
zhouzebiao <447523426>
2017-03-03 15:12:56 +0800

    api successful
    Signed-off-by: zhouzebiao <447523426>

--- django_restful_api/urls.py
+++ django_restful_api/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
-from django.contrib import admin
 
 from nlp_app import views as nlp_views
 
 urlpatterns = [
-                  url(r'^add/$', nlp_views.add, name='add'),
-                  url(r'^$', nlp_views.index, name='home'),
-                  url(r'^admin/', admin.site.urls),
+                  url(r'^v1/demo/word_seg/$', nlp_views.word_seg, name='word_seg'),
+                  url(r'^v1/demo/key/$', nlp_views.key, name='key'),
+                  # url(r'^$', nlp_views.index, name='home'),
+                  # url(r'^admin/', admin.site.urls),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- nlp_app/views.py
+++ nlp_app/views.py
@@ -3,22 +3,40 @@ from __future__ import print_function
 import json
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-import zaber_nlp.posseg
-
-
-def index(request):
-    return render(request, 'index.html')
+from zaber_nlp import posseg, analyse
 
 
-def add(request):
-    s = request.POST['q']
-    words = zaber_nlp.posseg.cut(s)
-    word_list = []
-    cate_list = []
-    for word, flag in words:
-        word_list.append(word)
-        cate_list.append(flag)
-    result = {'words': word_list, 'cate': list(set(cate_list))}
+# def index(request):
+#     return render(request, 'index.html', )
+
+
+@csrf_exempt
+def word_seg(request):
+    result = {}
+    if request.method == 'POST':
+        s = request.POST['q']
+        words = posseg.cut(s)
+        word_list = []
+        cate_list = []
+        for word, flag in words:
+            word_list.append(word)
+            cate_list.append(flag)
+        result = {'words': word_list, 'category': list(set(cate_list))}
+    return HttpResponse(json.dumps(result), content_type='application/json')
+
+
+@csrf_exempt
+def key(request):
+    result = {}
+    if request.method == 'POST':
+        s = request.POST['q']
+        words = analyse.Text_Rank(s, withWeight=True, allowPOS='n')
+        key_list = []
+        print(words)
+        for word, weight in words:
+            print(word, weight)
+            key_list.append([word, weight])
+        result = {'task_pd': key_list}
     return HttpResponse(json.dumps(result), content_type='application/json')
